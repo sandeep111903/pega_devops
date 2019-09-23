9f3047aace7753832b9ec86caefd3cfa821441d9
Raj <rajxeon@gmail.com>
2019-09-23 15:24:40 +0530

    Auto Commit : 09-23-2019_03-24-40

--- pusher.ps1
+++ pusher.ps1
@@ -2,5 +2,5 @@
 $command='git add -A'
 iex $command
 
-$command='git commit -m "Auto Commit : "'
+$command='git commit -m "Auto Commit : '+$(Get-Date -format "MM-dd-yyyy_hh-mm-ss")+'"'
 iex $command

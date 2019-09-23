e1cae4ed8929717e2a81e7833a6103cae8dbb88c
Raj <rajxeon@gmail.com>
2019-09-23 15:25:13 +0530

    Auto Commit : 09-23-2019_03-25-13

--- pusher.ps1
+++ pusher.ps1
@@ -4,3 +4,8 @@ iex $command
 
 $command='git commit -m "Auto Commit : '+$(Get-Date -format "MM-dd-yyyy_hh-mm-ss")+'"'
 iex $command
+
+
+$command='git -c diff.mnemonicprefix=false -c core.quotepath=false --no-optional-locks push -v --tags origin master:master'
+iex $command
+

ade6f54efc0490372f674d5ee2bfd44bda4d0590
Raj <rajxeon@gmail.com>
2019-09-23 15:36:04 +0530

    Auto Commit : 09-23-2019_03-36-04

--- listener.ps1
+++ listener.ps1
@@ -14,8 +14,9 @@ Register-ObjectEvent $watcher Created -SourceIdentifier FileCreated -Action {
 $name = $Event.SourceEventArgs.Name
 $changeType = $Event.SourceEventArgs.ChangeType
 $timeStamp = $Event.TimeGenerated
-Write-Host "This file: $name, was $changeType at $timeStamp!"
+Write-Host "This file: $name, was $changeType at $timeStamp!1"
 Out-File -filepath 'C:\Users\rajxe\OneDrive\Desktop\listener\changeLogs.txt' -append -inputobject "This file: $name, was $changeType at $timeStamp!"
+
 & ".\pusher.ps1"
 }
 
@@ -23,7 +24,7 @@ Register-ObjectEvent $watcher Deleted -SourceIdentifier FileDeletion -Action {
 $name = $Event.SourceEventArgs.Name
 $changeType = $Event.SourceEventArgs.ChangeType
 $timeStamp = $Event.TimeGenerated
-Write-Host "This file: $name, was $changeType at $timeStamp!"
+Write-Host "This file: $name, was $changeType at $timeStamp!2"
 Out-File -filepath 'C:\Users\rajxe\OneDrive\Desktop\listener\changeLogs.txt' -append -inputobject "This file: $name, was $changeType at $timeStamp!"
 }
 
@@ -31,7 +32,7 @@ Register-ObjectEvent $watcher Changed -SourceIdentifier FileChange -Action {
 $name = $Event.SourceEventArgs.Name
 $changeType = $Event.SourceEventArgs.ChangeType
 $timeStamp = $Event.TimeGenerated
-Write-Host "This file: $name, was $changeType at $timeStamp!"
+Write-Host "This file: $name, was $changeType at $timeStamp!3"
 Out-File -filepath 'C:\Users\rajxe\OneDrive\Desktop\listener\changeLogs.txt' -append -inputobject "This file: $name, was $changeType at $timeStamp!"
 }
 
